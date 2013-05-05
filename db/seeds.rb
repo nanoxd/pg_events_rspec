01bf2463cb5423eca0b01836b31a7df2ba14700e
Fernando Paredes <nanoid.xd@gmail.com>
2013-05-04 21:03:14 -0700

    Add time format for each project.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Event.create!([
+{
+  name: 'BugSmash',
+  location: 'Denver, CO',
+  price: 0.00,
+  description: "Join us for a fun evening of bug smashing! It's a good time",
+  starts_at: 100.days.from_now
+},
+  {
+  name: "Hackathon",
+  location: 'Austin, TX',
+  price: 15.00,
+  description: "Got a killer app idea you've been itching to work on? Well come on down",
+  starts_at: 120.days.from_now
+},
+  {
+  name: 'Kata Camp',
+  location: 'Dallas, TX',
+  price: 75.00,
+  description: 'Kata Camp is where developers go to practice their craft.',
+  starts_at: 150.days.from_now
+}
+])
+
+
+
+
+
